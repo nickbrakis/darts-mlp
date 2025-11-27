e7f2dc2ca6acb2aaf18ad72741487683e42befb9
nickbrakis <nibrakis@gmail.com>
2025-11-27 18:42:52 +0200

    update tags

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="darts-mlp",
-    version="1.1.0",
+    version="0.2.0",
     author="nickbrakis",
     author_email="nibrakis@gmail.com",
     description="Multi-Layer Perceptron (MLP) model for Darts time series forecasting",
